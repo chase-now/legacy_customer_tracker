c90e07127d27d3f0a709a55ccad5b5918eafc4b7
Prelang Builder <builder@prelang.com>
2015-12-04 23:57:48 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Product' to allow for create, update

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Product do
+  permit_params :AltProductCode, :AltProductDesc, :AltSearchCode, :Author, :CatAddon, :CatBook, :CatOther, :CatQuickGuide, :CatWebinarCD, :CatWebinarSubsc, :Cost, :Current, :DateAdded, :DownloadOnly, :InventoryID, :InventoryItem, :LegacyProduct, :LegCD, :LegInternational, :LegJustMan, :LegJustManDL, :LegManual, :LegNewCode, :LegPDFEmail, :LegUpgradeCode, :LegVol1Training, :LegVol2Training, :MultiPack, :Price, :PrintsInvoice, :ProductCode, :ProductDesc, :ProductParts, :SearchCode, :SendAnEmail, :Tag, :UnlockRequired
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
